74d179b62cc8c993131f3c0157cab12175e1346d
gardnerd06 <115792714+gardnerd06@users.noreply.github.com>
2022-12-28 14:10:44 -0500

    added bootstrap html and descriptions for project layout

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,17 @@
 console.log("name");
 
 // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+// Create a Dashboard with form inputs
+
+// search by city and return current and future conditions (5-day)
+
+//  Weather results persist in History
+
+// Results should be city name, date, icon that represents
+//  current weather, the temperature, humidity and wind speed
+
+// results should include a 5-day forcast
+
+// When I click a city from the history then you are
+// shown current and future conditions for that city.
