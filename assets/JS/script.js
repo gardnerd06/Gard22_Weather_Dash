120467c620f6a244a091cd93a7c487f4b808d804
gardnerd06 <115792714+gardnerd06@users.noreply.github.com>
2023-01-02 19:18:07 -0500

    added code for today's weather and future weathr

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -64,9 +64,61 @@ function getCity() {
 function getWeather() {
   var lat = $("#latitude").val();
   var lon = $("#longitude").val();
+  var count = "";
   let key = "d15a75a7f0fb2c83503cf38ba5a847c7";
   var weatherURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`;
   console.log("you're searching for weather");
+  var currentWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`;
+
+  fetch(currentWeather)
+    .then((todayWeather) => {
+      if (todayWeather.status === 200) {
+        return todayWeather.json();
+      } else {
+        throw new Error("Something went wrong on the API server!");
+      }
+    })
+    .then((today) => {
+      console.log(today);
+      var now = dayjs(today.dt * 1000).format("dddd, MMMM D, YYYY h:mm A");
+      var weathericon = today.weather[0].icon;
+      var sunrise = dayjs(today.sys.sunrise * 1000).format("h-mm-A");
+      var sunset = dayjs(today.sys.sunset * 1000).format("h-mm-A");
+      var windSpeed = today.wind.speed;
+      var mainWeather = today.weather[0].main;
+      var humidity = today.main.humidity;
+      var tempHi = today.main.temp_max;
+      var tempLow = today.main.temp_min;
+      var here = today.name;
+      var mainTemp = today.main.temp;
+
+      var todayForecast = `<div class="col">
+      <div class="card h-100">
+      <img
+      src="http://openweathermap.org/img/wn/${weathericon}@4x.png"
+      alt="Weather info"
+      width="100"
+      height="100"
+      class="d-inline-block align-text-top"
+      />
+      <div class="card-body">
+      <h5 class="card-title">${here}</h5>
+      <p  class="card-text">${now}</p>
+      <p  class="card-text">Forecast:  ${mainWeather}</p>
+      <p  class="card-text">Temp:  ${mainTemp} f</p>
+      <p  class="card-text">Temp High:  ${tempHi}</p>
+       <p  class="card-text">Temp Low:  ${tempLow}</p>
+       <p id ='humidity' class="card-text">Humidity:${humidity}%</p>
+       <p id ='precip' class="card-text">WindSpeed:  ${windSpeed} MPH</p>
+       <p id ='sunrise' class="card-text">SunRise: ${sunrise}</p>
+       <p id ='Sunset' class="card-text">SunSet: ${sunset}</p>
+       <p class="card-text">
+         <small class="text-muted">Last updated 3 mins ago</small>
+       </p>
+       </div>
+      `;
+      $("#weatherMain").prepend(todayForecast);
+    });
 
   fetch(weatherURL)
     .then((weather) => {
@@ -79,23 +131,15 @@ function getWeather() {
     .then((info) => {
       console.log(info);
       var days = info.list;
-
-      for (var i = 0; i < 5; i++) {
+      for (var i = 0; i < days.length; i++) {
         var city = info.city.name;
+        var time = dayjs(info.list[i].dt * 1000).format(
+          "dddd, MMMM D, YYYY h:mm A"
+        );
         var icon = info.list[i].weather[0].icon;
         var weatherNow = info.list[i].weather[0].main;
         var currtemp = info.list[i].main.temp;
-        var tempHi = info.list[i].main.temp_max;
-        var tempLow = info.list[i].main.temp_min;
-        var humidity = info.list[i].main.humidity;
-        var windSpeed = info.list[i].wind.speed;
         // var precip = "";
-        var sunrise = dayjs(info.city.sunrise * 1000).format("h-mm-A");
-        console.log(sunrise);
-
-        var sunset = dayjs(info.city.sunset * 1000).format("h-mm-A");
-        console.log(sunset);
-
         var weatherCard = `<div class="col">
       <div class="card h-100">
       <img
@@ -107,24 +151,16 @@ function getWeather() {
       />
       <div class="card-body">
       <h5 class="card-title">${city}</h5>
-      <p  class="card-text">${current}</p>
+      <p  class="card-text">${time}</p>
       <p  class="card-text">Forecast:  ${weatherNow}</p>
-      <p  class="card-text">Curr Temp:  ${currtemp} f</p>
-      <p  class="card-text">Today's High:  ${tempHi}</p>
-      <p  class="card-text">Today's Low:  ${tempLow}</p>
-      <p id ='humidity' class="card-text">Humidity:${humidity}</p>
-      <p id ='precip' class="card-text">WindSpeed:  ${windSpeed} MPH</p>
-      <p id ='sunrise' class="card-text">SunRise: ${sunrise}</p>
-      <p id ='Sunset' class="card-text">SunSet: ${sunset}</p>
-      <p class="card-text">
-        <small class="text-muted">Last updated 3 mins ago</small>
-      </p>
-      </div>
+      <p  class="card-text">Temp:  ${currtemp} f</p>
+      
       `;
+
         $("#weatherMain").append(weatherCard);
       }
     });
-  //
 }
 // city name, date, icon that represents
 //   current weather, the temperature, humidity and wind speed
+// {/*  */}
